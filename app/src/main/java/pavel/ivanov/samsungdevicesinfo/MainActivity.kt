587c8fd3088aeaf1cf8f1a58295860d97a4d4286
KilliLogan <ivanov-p-a@bk.ru>
2022-06-27 02:09:29 +0300

    Добавил bottomNavigatorMobile в layout и создал для него menu, добавил Toolbar в layout без menu и вложенностей - это только заготовки, для лучшей визуализации расположений view

--- app/src/main/java/pavel/ivanov/samsungdevicesinfo/MainActivity.kt
+++ app/src/main/java/pavel/ivanov/samsungdevicesinfo/MainActivity.kt
@@ -2,10 +2,29 @@ package pavel.ivanov.samsungdevicesinfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import pavel.ivanov.samsungdevicesinfo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bindingActivityMain: ActivityMainBinding //Обращение к элементам View через Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //Инициализация viewBinding
+        bindingActivityMain = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingActivityMain.root)
+
+        //Удаление строки состояния
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        if (savedInstanceState == null) {
+            //Подключение главного фрагмента
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.containerActivityMain, MobileFragment.newInstance())
+                .commit()
+        }
     }
 }
