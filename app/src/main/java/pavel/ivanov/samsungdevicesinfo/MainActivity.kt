fdff1c857062c95e89b550add329d4629cc39a09
KilliLogan <ivanov-p-a@bk.ru>
2022-07-03 02:29:08 +0300

    Добавил иконки в bottom_nav_menu, создал ресурс для изменения цвета активного элемента в bottom_nav_menu

--- app/src/main/java/pavel/ivanov/samsungdevicesinfo/MainActivity.kt
+++ app/src/main/java/pavel/ivanov/samsungdevicesinfo/MainActivity.kt
@@ -3,6 +3,7 @@ package pavel.ivanov.samsungdevicesinfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import pavel.ivanov.samsungdevicesinfo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,8 +17,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(bindingActivityMain.root)
 
         //Удаление строки состояния
-        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        this.window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
         if (savedInstanceState == null) {
             //Подключение главного фрагмента
@@ -25,6 +28,36 @@ class MainActivity : AppCompatActivity() {
                 .beginTransaction()
                 .replace(R.id.containerActivityMain, MobileFragment.newInstance())
                 .commit()
+        }
+
+        bindingActivityMain.viewPagerActivityMain.adapter = ViewPagerMainActivityAdapter(this)
+
+        bindingActivityMain.bottomNavigationMobile.selectedItemId = R.id.smartphones
+        bindingActivityMain.bottomNavigationMobile.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.accessories -> {
+                    //TODO ("объединить с viewPager")
+                    Toast.makeText(baseContext, "Accessories", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.tablets -> {
+                    //TODO ("объединить с viewPager")
+                    Toast.makeText(baseContext, "Tablets", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.smartphones -> {
+                    //TODO ("объединить с viewPager")
+                    Toast.makeText(baseContext, "Smartphone", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.laptops -> {
+                    //TODO ("объединить с viewPager")
+                    Toast.makeText(baseContext, "Laptops", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+
         }
     }
 }
---
+++ app/src/main/java/pavel/ivanov/samsungdevicesinfo/ViewPagerMainActivityAdapter.kt
@@ -0,0 +1,15 @@
+package pavel.ivanov.samsungdevicesinfo
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerMainActivityAdapter(private val fragmentManager: FragmentActivity) : FragmentStateAdapter(fragmentManager) {
+
+    private val mobileList = arrayOf(MobileFragment(), MobileFragment(), MobileFragment(), MobileFragment())
+
+    override fun getItemCount(): Int = mobileList.size
+
+    override fun createFragment(position: Int): Fragment = mobileList[position]
+
+}
