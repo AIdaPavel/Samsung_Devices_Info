514ea8bc51f2adf616e963e3f9452f0bfdd42256
KilliLogan <ivanov-p-a@bk.ru>
2022-07-12 13:20:01 +0300

    Переделал viewBindingDelegate, создал фрагменты под каждую категорию

---
+++ app/src/main/java/pavel/ivanov/samsungdevicesinfo/ui/mobile_fragments/AccessoriesFragment.kt
@@ -0,0 +1,10 @@
+package pavel.ivanov.samsungdevicesinfo.ui.mobile_fragments
+
+import androidx.fragment.app.Fragment
+import pavel.ivanov.samsungdevicesinfo.R
+import pavel.ivanov.samsungdevicesinfo.databinding.AccessoriesFragmentBinding
+import pavel.ivanov.samsungdevicesinfo.utils.viewBinding
+
+class AccessoriesFragment : Fragment(R.layout.accessories_fragment) {
+    private val binding by viewBinding(AccessoriesFragmentBinding::bind)
+}
